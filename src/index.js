9b6b7c5083001eedc06264d92cc2dc35c9ec087a
emesefedev <emesefedev@gmail.com>
2024-10-07 16:02:14 +0200

    feature: show and hide dropdown options

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import "./style.css";
 
 const dropdownButton = () => document.getElementById("dropdown-button");
+const dropdownOptions = () => document.getElementById("dropdown-options");
+
+let dropdownOptionsVisible = false;
 
 window.addEventListener("load", () => {
   dropdownButton().addEventListener("click", () => {
-    console.log("Show / Hide options");
+    dropdownOptionsVisible = !dropdownOptionsVisible;
+    if (dropdownOptionsVisible) {
+      showDropdownOptions();
+    } else {
+      hideDropdownOptions();
+    }
   });
 });
+
+function showDropdownOptions() {
+  dropdownOptions().classList.remove("hidden");
+}
+
+function hideDropdownOptions() {
+  dropdownOptions().classList.add("hidden");
+}
