b7f53e8aa67b02fc1dffceea5af8be92d01b7525
emesefe <emesefedev@gmail.com>
2024-10-07 16:15:29 +0200

    Dropdown (#1)
    * docs: update README
    
    * dropdown-layout
    
    * feature: show and hide dropdown options
    
    * feature: select and display selected option
    
    * test: add one more option

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import "./style.css";
 
-console.log("Hello");
+const dropdownButton = () => document.getElementById("dropdown-button");
+const dropdownOptions = () => document.getElementById("dropdown-options");
+
+let dropdownOptionsVisible = false;
+let selectedOption = null;
+
+window.addEventListener("load", () => {
+  dropdownButton().addEventListener("click", () => {
+    showOrHideDropdownOptions();
+  });
+
+  const dropdownOptionsArray = dropdownOptions().children;
+
+  for (const option of dropdownOptionsArray) {
+    option.addEventListener("click", (event) => {
+      selectDropdownOption(event.target);
+    });
+  }
+});
+
+function showDropdownOptions() {
+  dropdownOptions().classList.remove("hidden");
+}
+
+function hideDropdownOptions() {
+  dropdownOptions().classList.add("hidden");
+}
+
+function showOrHideDropdownOptions() {
+  dropdownOptionsVisible = !dropdownOptionsVisible;
+  if (dropdownOptionsVisible) {
+    showDropdownOptions();
+  } else {
+    hideDropdownOptions();
+  }
+}
+
+function selectDropdownOption(option) {
+  showOrHideDropdownOptions();
+
+  selectedOption = option;
+  displaySelectedOption(selectedOption);
+}
+
+function displaySelectedOption(option) {
+  dropdownButton().textContent = option.textContent;
+}
